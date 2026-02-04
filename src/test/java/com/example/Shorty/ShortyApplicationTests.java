189a563a5e397d3c07f1164e6e978a31afdf9823
sharveshsujay <sujaysharvesh098@gmail.com>
2026-02-04 22:14:06 +0530

    init commit

---
+++ src/test/java/com/example/Shorty/ShortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
